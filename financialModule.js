31e9fd4fd9adf5e94073bdffad505e2965fbafe9
Andrei Johann <andrei_johann@yahoo.com>
2019-03-24 23:43:25 -0300

    Acessing Backend api.exchangeratesapi.io

--- financialModule.js
+++ financialModule.js
@@ -1,18 +1,25 @@
 angular.module('Financial', [])
-    .factory('currencyConverter', function() {
+    .factory('currencyConverter', ['$http', function($http) {
         var currencies = ['USD', 'EUR', 'CNY'];
-        var usdToForeingRates = {
-            USD: 1,
-            EUR: 0.74,
-            CNY: 6.09
-        };
+        var usdToForeingRates = {};
         var convert = function(amount, inCurr, outCurr) {
             return amount * usdToForeingRates[outCurr] / usdToForeingRates[inCurr];
         };
+
+        var refresh = function () {
+            var url = 'https://api.exchangeratesapi.io/latest?base=USD&symbols=' + currencies.join(",");
+            return $http.get(url).then(function(response) {
+                usdToForeingRates = response.data.rates;
+                alert(JSON.stringify(response.data));
+                usdToForeingRates['USD'] = 1;
+            });
+        };
+
+        refresh();
 
         return {
             currencies: currencies,
             convert: convert
         };
 
-});
+}]);
